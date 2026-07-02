2a5eaf3a9d9d71b4759d0701cbf13c15c49e516a
Philippe Hountondji <hountondjiphilippe58@gmail.com>
2026-07-02 09:59:23 +0100

    backend complet

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: ['api/*']);
         $middleware->append(SecurityHeaders::class);
         $middleware->append(AuditLog::class);
+
         $middleware->alias([
             'role' => EnsureRole::class,
         ]);
